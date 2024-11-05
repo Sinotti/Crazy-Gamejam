86c646c581e665f6d570c5b04489712f68d204d3
Gabriel Sinotti <gabrielsntt13@gmail.com>
2024-11-05 20:20:16 -0300

    Units SO Update

--- Crazy-Gamejam/Assets/_Project/Logic/SO/BodyPartSO.cs
+++ Crazy-Gamejam/Assets/_Project/Logic/SO/BodyPartSO.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New BodyPartSO", menuName = "ScriptableObjects/BodyPartSO", order = 1)]
 public class BodyPartSO : ScriptableObject
 {
-    [SerializeField] private GameObject _UnitUIPrefab;
-    [SerializeField] private GameObject _UnitInGamePrefab;
+    [SerializeField] private GameObject _unitUIPrefab;
+    [SerializeField] private GameObject _unitInGamePrefab;
 
-    public GameObject Icon { get => _UnitUIPrefab; }
-    public GameObject Prefab { get => _UnitInGamePrefab; }
+    public GameObject UnitUIPrefab { get => _unitUIPrefab; }
+    public GameObject UnitInGamePrefab { get => _unitInGamePrefab; }
 }
